01337212fbee3e849be9a0857302344e0dadfe27
vimalraji <vimalraji.k@netkathir.com>
2026-08-03 15:01:04 +0530

    inital comment

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="container">
+      <h1>Happy Birthday!</h1>
+      <p>Wishing you a day full of joy, laughter, and beautiful memories.</p>
+    </main>
+  );
+}
+
+export default App;
